727c88d55d3155815692fe7576376543621bcc31
jeromie <jeromie@ajency.in>
2019-02-08 11:06:18 +0530

    lazyload revert

--- wp-content/themes/ajency-portfolio/footer.php
+++ wp-content/themes/ajency-portfolio/footer.php
@@ -90,7 +90,7 @@
 </script>
 <script type="text/javascript">
     window.lazySizesConfig = window.lazySizesConfig || {};
-      lazySizesConfig.loadMode = 0;
+      lazySizesConfig.loadMode = 3;
 </script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/combine.js" type="text/javascript">
 </script>
